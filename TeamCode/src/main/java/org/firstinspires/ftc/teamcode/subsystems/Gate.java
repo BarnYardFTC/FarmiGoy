31da982b4b007595337e909d4936d0405a79cc75
Petr <barnyard22993@gmail.com>
2026-06-18 11:45:29 +0300

    started working omn gate

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Gate.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Gate {
+
+    private Servo rightServo, leftServo;
+
+    private boolean isOpen = false;
+    private final double MIN = 0.88;
+
+    private final double MAX = 1;
+
+
+    public Gate(){
+        rightServo = hardwareMap.get(Servo.class, "rightGate");
+        leftServo = hardwareMap.get(Servo.class,"leftGate");
+        rightServo.setDirection(Servo.Direction.FORWARD);
+        leftServo.setDirection(Servo.Direction.REVERSE);
+        //call function to close gate
+
+
+    }
+    private void setPosition(double pos){
+
+        rightServo.setPosition(pos);
+        leftServo.setPosition(pos);
+    }
+    private void close(){
+        setPosition(MIN);
+        isOpen = false;
+    }
+    private void open() {
+        setPosition(MAX);
+        isOpen = true;
+    }
+    public boolean isOpen(){return isOpen;}
+//    public void displayTelemetry(){
+//
+//
+//    }
+
+
+}
