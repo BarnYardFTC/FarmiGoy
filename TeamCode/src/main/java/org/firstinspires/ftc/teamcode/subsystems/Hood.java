8067406e0ef1847fc28a79bcea1b02d2a63f9123
Ami <messidagoatedking@gmail.com>
2026-06-19 15:08:48 +0300

    manual hood control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Hood.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.Servo;
+import com.seattlesolvers.solverslib.command.Command;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+
+public class Hood extends SubsystemBase {
+
+    private Servo servo;
+
+
+    private final double MAX = 0.95;
+    private final double MIN = 0.35;
+
+
+    public Hood(){
+        servo =  hardwareMap.get(Servo.class, "shooterHood");
+        servo.setDirection(Servo.Direction.REVERSE);
+        servo.setPosition(MAX);
+
+
+    }
+    private void lower(){
+        if(servo.getPosition()-0.1>MIN){
+            servo.setPosition(servo.getPosition()-0.1);
+        }
+    }
+
+    private void raise(){
+        if(servo.getPosition()+0.1<MAX){
+            servo.setPosition(servo.getPosition()+0.1);
+        }
+    }
+
+    public double getPosition(){
+        return servo.getPosition();
+
+    }
+    public Command lowerCommand(){
+
+        return new InstantCommand(()-> lower(),this);
+    }
+    public Command raiseCommand(){
+
+        return new InstantCommand(()-> raise(), this);
+
+    }
+
+}
+
